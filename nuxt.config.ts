7a3dd6dba9aafb593915a86611dd3cb80a738f8f
SSE World <suryakantmaurya2007@gmail.com>
2024-07-19 19:50:41 +0530

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,16 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import { createResolver } from "@nuxt/kit"
+const { resolve } = createResolver(import.meta.url)
+
+export default defineNuxtConfig({
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+  alias: { '#sse-nui': resolve('./') },
+  components: [{ path: "#sse-nui/components", prefix: "U", pathPrefix: false }],
+  css: ["#sse-nui/assets/css/main.css"],
+  vite: {
+    optimizeDeps: {
+      include: ["vue3-smooth-dnd"],
+    },
+  },
+});
