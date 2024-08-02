ffd6b4d28d58ff269b195390500057929642998e
SSE World <suryakantmaurya2007@gmail.com>
2024-08-02 06:59:05 +0530

    Commit 21: Component Meta
    Component Meta has 1 issue Pending.
    This issue will be resolved within a week.
    
    Co-Authored-By: SSE Official <sseworld4@gmail.com>
    Co-Authored-By: SSE World <114757626+sseworld@users.noreply.github.com>

---
+++ modules/meta/runtime/server/api/component-meta.get.ts
@@ -0,0 +1,31 @@
+import { defineEventHandler, createError, appendHeader } from "h3";
+import { pascalCase } from "scule";
+// @ts-expect-error - Not resolve from tsconfig
+import components from "#sse-nui/component-meta/nitro";
+
+export default defineEventHandler((event) => {
+  // TODO: Replace via downstream config
+  appendHeader(event, "Access-Control-Allow-Origin", "*");
+
+  const componentName = (
+    (event.context.params && event.context.params["component?"]) ||
+    ""
+  ).replace(/\.json$/, "");
+
+  if (componentName) {
+    const meta = components[pascalCase(componentName)];
+    if (!meta) {
+      throw createError({
+        statusMessage: "Components not found!",
+        statusCode: 404,
+        data: {
+          description: "Please make sure you are looking for correct component",
+        },
+      });
+    }
+
+    return meta;
+  }
+
+  return components;
+});
